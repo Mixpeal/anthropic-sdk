8b8800019991c1e84baa0469263984cd64173f1a
Maksym <bienkowski.maksym@gmail.com>
2025-10-09 19:00:58 +0200

    chore: derive default & clone for types

--- src/types.rs
+++ src/types.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize, Serializer};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AnthropicUsage {
     pub input_tokens: Option<u32>,
     pub output_tokens: Option<u32>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AnthropicContentBlock {
     #[serde(rename = "type")]
     pub content_type: String,
@@ -23,7 +23,7 @@ pub struct AnthropicTextDelta {
     pub usage: Option<AnthropicUsage>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct AnthropicMessage {
     pub id: Option<String>,
     #[serde(rename = "type")]
@@ -43,7 +43,7 @@ pub struct AnthropicChatCompletionChunk {
     pub event_type: String,
     pub index: Option<usize>,
     pub delta: Option<AnthropicTextDelta>,
-    pub message: Option<AnthropicMessage>
+    pub message: Option<AnthropicMessage>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -74,9 +74,16 @@ impl Serialize for ToolChoice {
         S: Serializer,
     {
         match self {
-            ToolChoice::Auto => serde::Serialize::serialize(&serde_json::json!({"type": "auto"}), serializer),
-            ToolChoice::Any => serde::Serialize::serialize(&serde_json::json!({"type": "any"}), serializer),
-            ToolChoice::Tool(name) => serde::Serialize::serialize(&serde_json::json!({"type": "tool", "name": name}), serializer),
+            ToolChoice::Auto => {
+                serde::Serialize::serialize(&serde_json::json!({"type": "auto"}), serializer)
+            }
+            ToolChoice::Any => {
+                serde::Serialize::serialize(&serde_json::json!({"type": "any"}), serializer)
+            }
+            ToolChoice::Tool(name) => serde::Serialize::serialize(
+                &serde_json::json!({"type": "tool", "name": name}),
+                serializer,
+            ),
         }
     }
 }
